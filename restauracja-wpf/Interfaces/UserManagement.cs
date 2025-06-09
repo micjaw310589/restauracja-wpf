22f80f09540469c72820f48c582a6e962fb69f56
micjaw310589 <mj310589@student.polsl.pl>
2025-06-09 20:58:38 +0200

    wip: zmiana hasła, usunięcie usera

--- restauracja-wpf/Interfaces/UserManagement.cs
+++ restauracja-wpf/Interfaces/UserManagement.cs
@@ -1,4 +1,5 @@
-﻿using restauracja_wpf.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using restauracja_wpf.Data;
 using restauracja_wpf.Models;
 using restauracja_wpf.Services;
 using System;
@@ -6,32 +7,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 
 namespace restauracja_wpf.Interfaces
 {
     public class UserManagement
     {
+        private readonly GenericDataService<User> _userService;
+
+        public UserManagement(GenericDataService<User> userService)
+        {
+            _userService = userService;
+        }
+
         public async void AddUser(string firstname, string lastname, string login, string password, string restaurant, string role)
         {
-            GenericDataService<User> userService = new GenericDataService<User>(new RestaurantContextFactory());
-            GenericDataService<Role> roleService = new GenericDataService<Role>(new RestaurantContextFactory());
-            GenericDataService<Restaurant> restaurantService = new GenericDataService<Restaurant>(new RestaurantContextFactory());
-
-            var roles = await roleService.GetAll();
-            var foundRole = roles.Select(r => r).Where(r => r.Name == role);
-
-            var restaurants = await restaurantService.GetAll();     // wip
-            var foundRestaurant = restaurants.Select(r => r).Where(r => r.Name == restaurant);
-
-            await userService.Create(new User()
+            using (var context = new RestaurantContextFactory().CreateDbContext())
             {
-                FirstName = firstname,
-                LastName = lastname,
-                Login = login,
-                PasswordHash = password,
-                RoleId = foundRole.First().Id,
-                RestaurantId = foundRestaurant.First().Id
-            });
+                var foundRestaurant = await context.Restaurants
+                    .FirstOrDefaultAsync(r => r.Name == restaurant);
+                var foundRole = await context.Roles
+                    .FirstOrDefaultAsync(r => r.Name == role);
+
+                if (foundRestaurant == null)
+                    throw new Exception("Restaurant not found");
+                else if (foundRole == null)
+                    throw new Exception("Role not found");
+
+                await _userService.Create(new User()
+                {
+                    FirstName = firstname,
+                    LastName = lastname,
+                    Login = login,
+                    PasswordHash = password,
+                    RoleId = foundRole.Id,
+                    RestaurantId = foundRestaurant.Id
+                });
+            }
+
+        }
+
+        public async Task<IEnumerable<User>> GetMatchingUsers(string lastname)
+        {
+            using (var context = new RestaurantContextFactory().CreateDbContext())
+            {
+                IEnumerable<User> foundUsers = await context.Users
+                    .Where(u => u.LastName.Contains(lastname))
+                    .ToListAsync();
+
+                return foundUsers;
+            }
         }
     }
 }
--- restauracja-wpf/MainWindow.xaml.cs
+++ restauracja-wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Automation.Peers;
 using System.Windows.Controls;
@@ -32,7 +33,8 @@ public partial class MainWindow : Window
         cmbRole.DisplayMemberPath = "Name";
         cmbRole.SelectedValuePath = "Id";
         cmbRole.SelectedIndex = 0;
-
+         
+        // docelowo zamienić na wyszukiwanie restauracji po nazwie
         GenericDataService<Restaurant> restaurantService = new GenericDataService<Restaurant>(new RestaurantContextFactory());
         cmbRestaurant.ItemsSource = await restaurantService.GetAll();
         cmbRestaurant.DisplayMemberPath = "Name";
@@ -46,18 +48,112 @@ public partial class MainWindow : Window
             txtLogin.Text.IsNullOrEmpty() || txtPassword.Password.IsNullOrEmpty() ||
             txtConfirmPassword.Password.IsNullOrEmpty() ||
             cmbRole.SelectedValue.ToString().IsNullOrEmpty())
-            return;     // throw
+        {
+            MessageBox.Show("Please fill in all fields.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return; // throw new Exception("Please fill in all fields.");
+        }
         else if (txtPassword.Password != txtConfirmPassword.Password)
-            return;     // throw
+        {
+            MessageBox.Show("Passwords do not match.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return; // throw new Exception("Passwords do not match.");
+        }
+        else if (cmbRestaurant.SelectedValue == null)
+        {
+            MessageBox.Show("Please select a restaurant.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return; // throw new Exception("Please select a restaurant.");
+        }
+        else if (cmbRole.SelectedValue == null)
+        {
+            MessageBox.Show("Please select a role.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return; // throw new Exception("Please select a role.");
+        }
 
-        UserManagement userManagement = new();
-        userManagement.AddUser(
-            txtFirstname.Text,
-            txtLastname.Text,
-            txtLogin.Text,
-            txtPassword.Password,
-            cmbRestaurant.Text,
-            cmbRole.Text
-            );
+        string firstname = txtFirstname.Text;
+        string lastname = txtLastname.Text;
+        string login = txtLogin.Text;
+        string password = txtPassword.Password;
+        string restaurant = cmbRestaurant.Text;
+        string role = cmbRole.Text;
+
+        var messageBoxResult = MessageBox.Show($"Confirm User creation:\n" +
+            $"Firstname: {firstname}\n" +
+            $"Lastname: {lastname}\n" +
+            $"Login: {login}\n" +
+            $"Password: {password}\n" +
+            $"Role: {role}\n" +
+            $"Restaurant: {restaurant}", "Confirm", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+
+        if (messageBoxResult == MessageBoxResult.OK)
+        {
+            UserManagement userManagement = new(new GenericDataService<User>(new RestaurantContextFactory()));
+            userManagement.AddUser(
+                firstname,
+                lastname,
+                login,
+                password,
+                restaurant,
+                role
+                );
+        }
+    }
+
+    private async void btnSearchUser_ClickAsync(object sender, RoutedEventArgs e)
+    {
+        UserManagement userManagement = new(new GenericDataService<User>(new RestaurantContextFactory()));
+        string lastname = txtSearchUser.Text;
+
+        if (string.IsNullOrEmpty(lastname))
+        {
+            MessageBox.Show("Please enter a lastname to search.", "Validation Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return; // throw new Exception("Please enter a lastname to search.");
+        }
+
+        var users = await userManagement.GetMatchingUsers(lastname);
+
+        if (users == default || users == null)
+        {
+            MessageBox.Show("No users found with the given lastname.", "Search Result", MessageBoxButton.OK, MessageBoxImage.Information);
+            return; // throw new Exception("No users found with the given lastname.");
+        }
+
+        lbxUserSearchResults.Items.Clear();
+        lbxUserSearchResults.SelectedItem = null;
+        foreach (var user in users)
+        {
+            lbxUserSearchResults.Items.Add($"{user.Id} {user.FirstName} {user.LastName} (login: {user.Login})");
+        }
+    }
+
+    private async void OnSelected(object sender, SelectionChangedEventArgs e)
+    {
+        if (lbxUserSearchResults.SelectedItem != null)
+        {
+            string[] selectedUser = lbxUserSearchResults.SelectedItem.ToString().Split(" ");
+            try
+            {
+                int id = Convert.ToInt32(selectedUser[0]);
+                var genericDataService = new GenericDataService<User>(new RestaurantContextFactory());
+                var user = await genericDataService.Get(id);
+                if (user == null)
+                {
+                    MessageBox.Show("User not found.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return; // throw new Exception("User not found.");
+                }
+                else
+                {
+                    txtFirstname.Text = user.FirstName;
+                    txtLastname.Text = user.LastName;
+                    txtLogin.Text = user.Login;
+                    txtPassword.Password = user.PasswordHash; // Note: This should not be done in production, passwords should not be retrievable.
+                    cmbRole.SelectedValue = user.RoleId;
+                    cmbRestaurant.SelectedValue = user.RestaurantId;
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Failed Id convertion.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return; // throw new Exception("Invalid user selection. Please select a valid user.");
+            }
+        }
     }
 }
